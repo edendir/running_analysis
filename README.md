db7256d4f7d0dcb563412f94bf356b361fd0c102
Robbie Lannon <robbielannon@gmail.com>
2025-09-11 16:00:27 -0500

    Too many changes. Too excited to start. Part 1

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # running_analysis
-Analysis of my running activities on Strava. Goal to determine peak race times.
+# Analysis of my running activities on Strava. Goal to determine peak race times.
+
+A cloud-native data project analyzing my training data over the last number of years. The goal of this is to track my progress and also determine what my possible peak race times could be at each distance.
+
+## Technical Goals
+- Ingest training data from Strava API
+- Orchestrate ETL pipeline using GCP Composer
+- Store structured data in Cloud Storage
+- Apply predictive modeling to forecast race performance
+- Visualize trends across training cycles/phases
+
+## Stack
+- GCP
+- Terraform
+- Airflow (Composer)
+- Docker
+- Python
---
+++ scripts/fetch_data.py
@@ -0,0 +1,48 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("STRAVA_CLIENT_ID")
+CLIENT_SECRET = os.getenv("STRAVA_CLIENT_SECRET")
+REFRESH_TOKEN = os.getenv("STRAVA_REFRESH_TOKEN")
+
+def get_access_token():
+    url = "https://www.strava.com/oauth/token"
+    payload = {
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "refresh_token": REFRESH_TOKEN,
+        "grant_type": "refresh_token"
+    }
+    
+    response = requests.post(url, data=payload)
+    response.raise_for_status()
+    return response.json()["access_token"]
+
+def get_runs(access_token, max_pages=5):
+    url = "https://www.strava.com/api/v3/athlete/activities"
+    headers = {"Authorization": f"Bearer {access_token}"}
+    all_runs = []
+    
+    for page in range(1, max_pages + 1):
+        params = {"per_page": 200, "page": 1}
+
+        response = requests.get(url, headers=headers, params=params)
+        
+        response.raise_for_status()
+        activities = response.json()
+        runs = [activity for activity in activities if activity["type"] == "Run"]
+        all_runs.extend(runs)
+
+        if len(activities) < 200:
+            break
+
+    return all_runs
+
+if __name__ == "__main__":
+    token = get_access_token()
+    runs = get_runs(token)
+    for run in runs[:5]: 
+        print(f"Run on {run['start_date']}: {run['distance']} meters")
